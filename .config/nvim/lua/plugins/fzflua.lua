28c3cc3d229c41749a336a1d403c47fc1d003b19
Simon Lejeune <lejeunesimon.91@gmail.com>
2022-10-18 09:49:29 +0100

    vim: plugins: restore last search with fzf-lua

--- .config/nvim/lua/plugins/fzflua.lua
+++ .config/nvim/lua/plugins/fzflua.lua
@@ -3,6 +3,6 @@ local options = { noremap = true, silent = true }
 
 set_keymap('n', '<c-p>', "<cmd>lua require('fzf-lua').git_files()<CR>", options)
 set_keymap('n', ';', "<cmd>lua require('fzf-lua').buffers()<CR>", options)
-set_keymap('n', '<leader>/', ":FzfLua live_grep_glob<CR>", options)
+set_keymap('n', '<leader>/', ":FzfLua live_grep_resume<CR>", options)
 set_keymap('n', '<leader>fof', "<cmd>lua require('fzf-lua').oldfiles()<CR>", options)
 set_keymap('n', '<F12>', ":FzfLua lsp_document_symbols<CR>", options)
