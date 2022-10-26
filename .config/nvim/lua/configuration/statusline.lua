c66466d7488369ec7c7af9c98719cbecae834a3d
Simon Lejeune <lejeunesimon.91@gmail.com>
2022-10-26 09:29:56 +0100

    vim: configuration: status line
    remove all those back slashes and add the only whitespace needed

--- .config/nvim/lua/configuration/statusline.lua
+++ .config/nvim/lua/configuration/statusline.lua
@@ -1,21 +1,16 @@
 vim.cmd(
   [[
-set statusline=\
+set statusline=
 set statusline+=%f   " Path to the file
 set statusline+=%w   " If we are in a preview window
 set statusline+=%r   " Readonly flag
 set statusline+=%m   " Modified flag
 set statusline+=%=   " Switch to the right side
 set statusline+=%{FugitiveStatusline()}
-set statusline+=\
-set statusline+=\
-set statusline+=\
+set statusline+=\    " Whitespace
 set statusline+=%l   " Current line
 set statusline+=/    " Separator
 set statusline+=%L   " Total lines
-set statusline+=\
-set statusline+=\
-set statusline+=\
 set statusline+=%c   " column
 ]]
 )
