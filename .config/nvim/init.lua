a7012602b2f7e55a3e73e8ec4bc4effca2854f14
Simon Lejeune <lejeunesimon.91@gmail.com>
2022-11-07 10:10:45 +0000

    vim: plugins: configure tree tree-sitter
    and format plugins list

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,7 @@
 -- inspired by https://github.com/talha-akram/anvil/blob/220e21e46154da965b0ce16b497a0d99355a7570/lua/plugins/lspconfig.lua
 -- https://github.com/ibhagwan/nvim-lua/blob/6fc6379046d3abbfabfcd26c459b7ded87e32c76/lua/plugins/fzf-lua/mappings.lua
+-- https://github.com/NullVoxPopuli/dotfiles/blob/main/home/.config/nvim/lua/plugin-config/lsp.lua
+--
 require('configuration')
 if (vim.fn.exists('$SUDO_USER') == 0) then
   require('plugins')
